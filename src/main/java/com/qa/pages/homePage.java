819b272da2682bb40815b6437639c59a74e2ee13
ssrinivassrs@gmail.com <ssrinivassrs@gmail.com>
2021-08-25 21:20:33 +1000

    Added search page for TestNg code

--- src/main/java/com/qa/pages/homePage.java
+++ src/main/java/com/qa/pages/homePage.java
@@ -1,5 +1,14 @@
 package com.qa.pages;
 
 public class homePage {
-
+	public void loginpage() {
+	
+	System.out.println("It is a home pae");
+	}
+	
+	public void loginHomePage(){
+		
+		System.out.println("It is a home pae");
+	}
 }
+
